1bdeffea06cf0479f06fd68f23d1e5c4510e9af1
tomflynn1999 <38897673+tomflynn1999@users.noreply.github.com>
2018-06-22 11:43:35 -0400

    Add files via upload
    Dog inheritance

--- app.py
+++ app.py
@@ -4,31 +4,66 @@ app = Flask(__name__)
 
 
 @app.route('/')
+# Parent class
+class Pets:
+
+    dogs = []
+
+    def __init__(self, dogs):
+        self.dogs = dogs
+
+
+# Parent class
 class Dog:
 
-    # Class Attribute
+    # Class attribute
     species = 'mammal'
 
-    # Initializer / Instance Attributes
+    # Initializer / Instance attributes
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    # Instance method
+    def description(self):
+        return self.name, self.age
 
-# Instantiate the Dog object
-jake = Dog("Jake", 7)
-doug = Dog("Doug", 4)
-william = Dog("William", 5)
+    # Instance method
+    def speak(self, sound):
+        return "%s says %s" % (self.name, sound)
+
+    # Instance method
+    def eat(self):
+        self.is_hungry = False
 
 
-# Determine the oldest dog
-def get_biggest_number(*args):
-    return max(args)
+# Child class (inherits from Dog class)
+class RussellTerrier(Dog):
+    def run(self, speed):
+        return "%s runs %s" % (self.name, speed)
 
+
+# Child class (inherits from Dog class)
+class Bulldog(Dog):
+    def run(self, speed):
+        return "%s runs %s" % (self.name, speed)
+
+# Create instances of dogs
+my_dogs = [
+    Bulldog("Tom", 6),
+    RussellTerrier("Fletcher", 7),
+    Dog("Larry", 9)
+]
+
+# Instantiate the Pets class
+my_pets = Pets(my_dogs)
 
 # Output
-print("The oldest dog is {} years old.".format(
-    get_biggest_number(jake.age, doug.age, william.age)))
+print("I have {} dogs.".format(len(my_pets.dogs)))
+for dog in my_pets.dogs:
+    print("{} is {}.".format(dog.name, dog.age))
+
+print("And they're all {}s, of course.".format(dog.species))
 
 
 if __name__ == '__main__':
